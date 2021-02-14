e6652e1fb042cd584646d983e0b30bc14a0f52e5
Felipe Marinho <felipmarinho@gmail.com>
2021-02-14 15:23:14 -0300

    Add components in MenuFull

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,8 +8,13 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined'
 
 import * as S from './styles'
 import Logo from '../Logo'
+import Button from '../Button'
 
-const Menu = () => {
+export type MenuProps = {
+  username?: string
+}
+
+const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <S.Wrapper>
@@ -30,6 +35,27 @@ const Menu = () => {
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
+        </S.MenuNav>
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign up">
+              Sign up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   )
