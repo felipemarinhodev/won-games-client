055b5690c2c98fe132dc43ec5ec3ef28233c1be5
Felipe Marinho <felipmarinho@gmail.com>
2021-02-25 06:43:11 -0300

    add tests of Banner component

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1024x580',
+  title: 'Defy death',
+  subTitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<Banner {...props} />)
+    // verificar se o title existe renderizado (.toBeInTheDocument())
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+    // verificar se o subtitle existe renderezizado
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+    // verificar se o image existe renderezizado
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+  })
+})
