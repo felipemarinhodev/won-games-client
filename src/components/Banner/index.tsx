2e0df499b1da7bf0b473b561a8fc9eacd6f43ddb
Felipe Marinho <felipmarinho@gmail.com>
2021-03-04 06:38:39 -0300

    add the Ribbon component in the Banner component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,4 +1,5 @@
 import Button from 'components/Button'
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
 import * as S from './styles'
 
 export type BannerProps = {
@@ -7,6 +8,9 @@ export type BannerProps = {
   subTitle: string
   buttonLink: string
   buttonLabel: string
+  ribbon?: React.ReactNode
+  ribbonSize?: RibbonSizes
+  ribbonColor?: RibbonColors
 }
 
 const Banner = ({
@@ -14,9 +18,17 @@ const Banner = ({
   title,
   subTitle,
   buttonLink,
-  buttonLabel
+  buttonLabel,
+  ribbon,
+  ribbonSize,
+  ribbonColor
 }: BannerProps) => (
   <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon size={ribbonSize} color={ribbonColor}>
+        {ribbon}
+      </Ribbon>
+    )}
     <S.Image src={img} role="img" aria-label={title} />
 
     <S.Caption>
