784915d68f9d629abcf9fd401869c30f247a6727
Felipe Marinho <felipmarinho@gmail.com>
2021-03-19 07:05:05 -0300

    export props and change default color of the arrow

--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -5,7 +5,7 @@ import GameCard, { GameCardProps } from 'components/GameCard'
 import Slider, { SliderSettings } from 'components/Slider'
 import * as S from './styles'
 
-type GameCardSliderProps = {
+export type GameCardSliderProps = {
   items: GameCardProps[]
   color?: 'white' | 'black'
 }
@@ -47,7 +47,7 @@ const settings: SliderSettings = {
     }
   ]
 }
-const GameCardSlider = ({ items, color = 'black' }: GameCardSliderProps) => (
+const GameCardSlider = ({ items, color = 'white' }: GameCardSliderProps) => (
   <S.Wrapper color={color}>
     <Slider settings={settings}>
       {items.map((item, index) => (
