0c236e2a2fd296fe5e269fb3f1192937919f3d64
Felipe Marinho <felipmarinho@gmail.com>
2021-03-19 06:33:35 -0300

    add test for GameCardSlider component

---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,78 @@
+import { screen } from '@testing-library/dom'
+import 'match-media-mock'
+import { renderWithTheme } from 'utils/tests/helpers'
+import GameCardSlider from '.'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
+describe('<GameCardSlider />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(container.querySelector('.slick-track')).toBeInTheDocument()
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(
+      items.length
+    )
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+
+    expect(screen.getByLabelText(/next games/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/previous games/i)).toBeInTheDocument()
+  })
+})
