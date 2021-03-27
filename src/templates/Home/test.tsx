cdca33e1d55d2e50f44d30bddb00df6fc0ad4096
Felipe Marinho <felipmarinho@gmail.com>
2021-03-27 09:50:03 -0300

    add tests to sections

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,8 +1,28 @@
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 import Home from '.'
 
 describe('<Home />', () => {
-  it('should render the heading', () => {
-    render(<Home />)
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /contact/i })
+    ).toBeInTheDocument()
+  })
+  it('should render the sections', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
   })
 })
