656d6d8c37796ff6c1f1b225bae2e42d7b1fc825
Felipe Marinho <felipmarinho@gmail.com>
2021-03-28 18:34:31 -0300

    improve index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from '../components/BannerSlider/mock'
+import gamesMock from '../components/GameCardSlider/mock'
+import highlightMock from '../components/Highlight/mock'
 
-export default function Index() {
-  return <Home />
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+// ATENÇÃO
+// os métodos getStaticProps/getServerSidePros SÓ FUNCIONAM EM PAGES
+
+// https://nextjs.org/docs/basic-features/pages
+// getStaticProps(Recommended) => gerar estático em build time
+// getServerSideProps(Recommended) => gerar via ssr a cada request
+// getInitialProps => gerar vai ssr a cada request
+export function getStaticProps() {
+  // faz lógica
+  // pode buscar dados numa API
+  // fazer calculo|leitura de context
+
+  // retorno dos dados
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcommingHighlight: highlightMock,
+      upcommingGames: gamesMock,
+      upcommingMoreGames: gamesMock,
+      freeHighlight: highlightMock,
+      freeGames: gamesMock
+    }
+  }
 }
