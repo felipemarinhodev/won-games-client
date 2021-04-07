6fa3b73445c1fb8d770990f7e75f1533a61a68f0
Felipe Marinho <felipmarinho@gmail.com>
2021-04-07 06:49:59 -0300

    refactor the path of the fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,23 +19,25 @@ const GlobalStyles: GlobalStyleComponent<
     font-weight: 300;
     font-display: swap;
     src: local('Poppins Light'), local('Poppins-Light'),
-        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'), /* Super Modern Browsers */
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
   }
+
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 400;
     font-display: swap;
     src: local('Poppins Regular'), local('Poppins-Regular'),
-        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
   }
+
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 600;
     font-display: swap;
     src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
-        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'), /* Super Modern Browsers */
+        url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
   }
 
   * {
@@ -59,11 +61,14 @@ const GlobalStyles: GlobalStyleComponent<
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+
       ${!removeBg &&
       css`
         background-color: ${theme.colors.mainBg};
       `}
     }
   `}
+
 `
+
 export default GlobalStyles
