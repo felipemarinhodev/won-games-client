ba49a8d1e185a9cc96bd36a2ee931c067e800b54
Felipe Marinho <felipmarinho@gmail.com>
2021-05-21 06:29:05 -0300

    test ensure Auth template will receive fields

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -1,14 +1,41 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    // const { container } =
-    render(<Auth />)
+  it('should render logos, title, subtitle, footer and children', () => {
+    renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+    // verificar se tem 2 logos
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
 
-    // expect(container.firstChild).toMatchSnapshot()
+    // verificar se tem o heading principal do banner
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in on place/i
+      })
+    ).toBeInTheDocument()
+
+    // verificar se tem o subtitle
+    expect(
+      screen.getByRole('heading', {
+        name: /WON is the best and most complete gaming platform./i
+      })
+    ).toBeInTheDocument()
+
+    // verificar se tem o title do content
+    expect(
+      screen.getByRole('heading', { name: /Auth title/i })
+    ).toBeInTheDocument()
+
+    // verificar se o children tá sendo renderizado
+    expect(
+      screen.getByRole('textbox').parentElement?.parentElement
+    ).toBeInTheDocument()
   })
 })
