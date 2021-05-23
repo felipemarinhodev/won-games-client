0b86229400b7b3e6bc502bb1388abedcedab422d
Felipe Marinho <felipmarinho@gmail.com>
2021-05-23 04:59:34 -0300

    test: ensure FormSignIn have the structure

---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/dom'
+import { renderWithTheme } from 'utils/tests/helpers'
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render the form', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByPlaceholderText(/e-mail/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+    expect(
+      screen.getByRole('link', { name: /forgot your password?/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text and link to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    // check text
+    expect(screen.getByText(/Don't have an account?/i)).toBeInTheDocument()
+    // check link
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+  })
+})
