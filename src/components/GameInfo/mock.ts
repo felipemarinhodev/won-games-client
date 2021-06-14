54832a39aeb02f12be6ffe8cc7390af2b878aaa9
Felipe Marinho <felipmarinho@gmail.com>
2021-06-14 07:58:13 -0300

    test: add GameInfo component

---
+++ src/components/GameInfo/mock.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'Borderlands 3',
+  price: '210,00',
+  description:
+    'Experience the epic space strategy games that redefined the RTS genre. Control your fleet and build and armada across more then 30 single-player missions.'
+}
