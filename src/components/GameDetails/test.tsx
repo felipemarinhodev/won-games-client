683856457baed5bbd53c8bf033e215400df921b7
Felipe Marinho <felipmarinho@gmail.com>
2021-06-18 05:40:49 -0300

    test: ensure props are correct

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -6,7 +6,10 @@ import GameDetails, { GameDetailProps } from '.'
 const props: GameDetailProps = {
   developer: 'Different Tales',
   platforms: ['windows', 'mac', 'linux'],
-  releaseDate: '2020-11-21T23:00:00'
+  releaseDate: '2020-11-21T23:00:00',
+  rating: 'BR0',
+  genres: ['Role-playing', 'Narrative'],
+  publisher: '2K'
 }
 
 describe('<GameDetails />', () => {
@@ -41,9 +44,40 @@ describe('<GameDetails />', () => {
     expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
   })
+
+  it('should render free rating when BR0', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/free/i)).toBeInTheDocument()
+  })
+
+  it('should render 18+ rating when BR18', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR18" />)
+
+    expect(screen.getByText(/18\+/i)).toBeInTheDocument()
+  })
+
+  it('should render 16+ rating when BR16', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR16" />)
+
+    expect(screen.getByText(/16\+/i)).toBeInTheDocument()
+  })
+
   it('should render the formated date', () => {
     renderWithTheme(<GameDetails {...props} />)
 
     expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
+  })
+
+  it('should render a list of genres', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText('Role-playing / Narrative')).toBeInTheDocument()
+  })
+
+  it('should render the publisher as 2K', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText('2K')).toBeInTheDocument()
   })
 })
