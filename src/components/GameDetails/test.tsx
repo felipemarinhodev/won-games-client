dfe80f07c49ae8559dbf2eb3238893323ef64cb1
Felipe Marinho <felipmarinho@gmail.com>
2021-06-17 22:18:29 -0300

    add test for the component

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -1,9 +1,49 @@
+import { screen } from '@testing-library/dom'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import GameDetails from '.'
+import GameDetails, { GameDetailProps } from '.'
+
+const props: GameDetailProps = {
+  developer: 'Different Tales',
+  platforms: ['windows', 'mac', 'linux'],
+  releaseDate: '2020-11-21T23:00:00'
+}
 
 describe('<GameDetails />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<GameDetails />)
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Developer/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /release date/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /platforms/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /publisher/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
+  })
+
+  it('should render platforms icons', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
+  })
+  it('should render the formated date', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
   })
 })
