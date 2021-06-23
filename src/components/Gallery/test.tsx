377b68b54e2714e840a3eefb14a0f9b79220f7c0
Felipe Marinho <felipmarinho@gmail.com>
2021-06-23 08:31:21 -0300

    test: component Gallery

---
+++ src/components/Gallery/test.tsx
@@ -0,0 +1,21 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Gallery from '.'
+
+import mockItems from './mock'
+
+describe('<Gallery />', () => {
+  it('should render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute('src', mockItems[0].src)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    ).toHaveAttribute('src', mockItems[1].src)
+  })
+})
