d3da31323e2317303790be92ba7b0a6ac0de74e5
Felipe Marinho <felipmarinho@gmail.com>
2021-06-23 10:01:46 -0300

    test: ensure when click in the image will open a modal

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,5 +1,5 @@
 import 'match-media-mock'
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Gallery from '.'
@@ -17,5 +17,22 @@ describe('<Gallery />', () => {
     expect(
       screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
     ).toHaveAttribute('src', mockItems[1].src)
+  })
+
+  it('should render the open modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // selecionar o nosso modal
+    const modal = screen.getByLabelText('modal')
+    // verificar se o menu está escondido
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
+
+    // clicar no botão de abrir o modal e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
   })
 })
