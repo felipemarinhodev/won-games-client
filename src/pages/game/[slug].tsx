c3b89fcc2f3e68a36911c2350a98c6e85caf6422
Felipe Marinho <felipmarinho@gmail.com>
2021-07-09 22:19:01 -0300

    using getStaticProps for recover data and getStaticPaths to define paths

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,5 +1,21 @@
-import Game from 'templates/Game'
+import Game, { GameTemplateProps } from 'templates/Game'
 
-export default function Index() {
-  return <Game />
+export default function Index(props: GameTemplateProps) {
+  return <Game {...props} />
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { slug: 'ghost-of-tsushima' } }],
+    fallback: false
+  }
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      cover:
+        'https://www.denofgeek.com/wp-content/uploads/2020/07/ghost-of-tsushima-ps5-1.jpg'
+    }
+  }
 }
