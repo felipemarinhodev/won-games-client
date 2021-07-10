ec7c875645dfaf12fe4b93d458b39ed3cc4319ef
Felipe Marinho <felipmarinho@gmail.com>
2021-07-09 22:16:39 -0300

    add props to component

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,13 +1,13 @@
 import Base from 'templates/Base'
 import * as S from './styles'
 
-const Game = () => (
+export type GameTemplateProps = {
+  cover: string
+}
+
+const Game = ({ cover }: GameTemplateProps) => (
   <Base>
-    <S.Cover
-      src="https://www.denofgeek.com/wp-content/uploads/2020/07/ghost-of-tsushima-ps5-1.jpg"
-      role="image"
-      aria-label="cover"
-    />
+    <S.Cover src={cover} role="image" aria-label="cover" />
   </Base>
 )
 
