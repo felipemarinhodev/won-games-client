4bc7dcdc98e0845f0f823d651664bbc361ae0749
Felipe Marinho <felipmarinho@gmail.com>
2021-07-10 15:21:07 -0300

    include SectionDescription and Description components and set style

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,4 +1,5 @@
 import Base from 'templates/Base'
+import TextContent from 'components/TextContent'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import * as S from './styles'
@@ -7,9 +8,10 @@ export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
+  description: string
 }
 
-const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
     <S.Main>
@@ -20,6 +22,10 @@ const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
       <S.SectionGallery>
         {!!gallery && <Gallery items={gallery} />}
       </S.SectionGallery>
+
+      <S.SectionDescription>
+        <TextContent title="Description" content={description} />
+      </S.SectionDescription>
     </S.Main>
   </Base>
 )
