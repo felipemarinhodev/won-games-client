51d917635f99038b677afb55108611d1979a5b4b
Felipe Marinho <felipmarinho@gmail.com>
2021-08-06 09:35:04 -0300

    add Wishlist component

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,16 @@
+import Base from 'templates/Base'
+import Heading from 'components/Heading'
+import { Container } from 'components/Container'
+import React from 'react'
+
+const Wishlist = () => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+    </Container>
+  </Base>
+)
+
+export default Wishlist
---
+++ src/templates/Wishlist/test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+
+// import Wishlist from '.'
+
+describe('<Wishlist />', () => {
+  it('should render the heading', () => {
+    render(<></>)
+  })
+})
