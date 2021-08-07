0251459927f40c9f45b5f33343223ec2763367c8
Felipe Marinho <felipmarinho@gmail.com>
2021-08-07 10:46:05 -0300

    set styled in the component

--- src/components/Empty/test.tsx
+++ src/components/Empty/test.tsx
@@ -10,7 +10,7 @@ const props = {
 
 describe('<Empty />', () => {
   it('should render correctly', () => {
-    renderWithTheme(<Empty {...props} hasLink />)
+    const { container } = renderWithTheme(<Empty {...props} hasLink />)
     expect(
       screen.getByRole('image', {
         name: /a gamer in a coach playing videogame/i
@@ -25,6 +25,7 @@ describe('<Empty />', () => {
     expect(
       screen.getByRole('link', { name: /go back to store/i })
     ).toHaveAttribute('href', '/')
+    expect(container.parentElement).toMatchSnapshot()
   })
   it(' should not render link when hasLink is not passed', () => {
     renderWithTheme(<Empty {...props} />)
