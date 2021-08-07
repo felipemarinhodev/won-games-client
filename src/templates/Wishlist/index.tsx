0400cb8c2bef831652513995452513bbf6cf5571
Felipe Marinho <felipmarinho@gmail.com>
2021-08-07 08:09:02 -0300

    add Divider component

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -2,10 +2,11 @@ import Base from 'templates/Base'
 
 import Heading from 'components/Heading'
 import Showcase from 'components/Showcase'
-import { Container } from 'components/Container'
 import GameCard, { GameCardProps } from 'components/GameCard'
-import { HighlightProps } from 'components/Highlight'
 import { Grid } from 'components/Grid'
+import { Divider } from 'components/Divider'
+import { Container } from 'components/Container'
+import { HighlightProps } from 'components/Highlight'
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[]
@@ -29,6 +30,7 @@ const Wishlist = ({
           <GameCard key={`wishlist-${index}`} {...games} />
         ))}
       </Grid>
+      <Divider />
     </Container>
     <Showcase
       title="You may like these games"
