a5d13f1da21313976f3b0844536d0ec81637c8be
Felipe Marinho <felipmarinho@gmail.com>
2021-08-07 08:14:31 -0300

    add Divider component

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,12 +1,14 @@
 import Base from 'templates/Base'
+import Showcase from 'components/Showcase'
 import TextContent from 'components/TextContent'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import GameDetails, { GameDetailProps } from 'components/GameDetails'
+import { Divider } from 'components/Divider'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
+
 import * as S from './styles'
-import Showcase from 'components/Showcase'
 
 export type GameTemplateProps = {
   cover: string
@@ -46,6 +48,7 @@ const Game = ({
 
       <S.SectionGameDetails>
         <GameDetails {...details} />
+        <Divider />
       </S.SectionGameDetails>
 
       <Showcase
