fd94143d03dabeaf24587f8e63a1f3295c89c326
Felipe Marinho <felipmarinho@gmail.com>
2021-08-07 06:49:42 -0300

    add Showcase component and Wishlist props

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,15 +1,31 @@
 import Base from 'templates/Base'
-import Heading from 'components/Heading'
-import { Container } from 'components/Container'
-import React from 'react'
 
-const Wishlist = () => (
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
   </Base>
 )
 
