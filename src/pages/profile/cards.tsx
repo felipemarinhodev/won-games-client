077c75fd881cd27958e7415cbfcd5888c8a956ff
Felipe Marinho <felipmarinho@gmail.com>
2021-08-20 11:06:09 -0300

    refactor  to use getServerSideProps

--- src/pages/profile/cards.tsx
+++ src/pages/profile/cards.tsx
@@ -1,12 +1,20 @@
-import CardsList from 'components/CardsList'
+import CardsList, { CardsListProps } from 'components/CardsList'
 import Profile from 'templates/Profile'
 
 import mockCards from '../../components/PaymentOptions/mock'
 
-export default function Cards() {
+export default function Cards({ cards }: CardsListProps) {
   return (
     <Profile>
-      <CardsList cards={mockCards} />
+      <CardsList cards={cards} />
     </Profile>
   )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      cards: mockCards
+    }
+  }
 }
