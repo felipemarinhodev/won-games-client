9ab0f0756b8cfc740ac2f999bb99c4a05302663b
felipe <felipmarinho@gmail.com>
2021-10-03 07:46:08 -0300

    ensure handle the Dropdown list to show and hide content

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -1,11 +1,30 @@
+import { screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
-
 import Dropdown from '.'
 
 describe('<Dropdown />', () => {
-  it('should render correctly', () => {
-    renderWithTheme(<Dropdown title={'Click here'}>Content</Dropdown>)
+  beforeEach(() => {
+    const title = <h1 aria-label="toogle dropdown">Click here</h1>
+    renderWithTheme(
+      <Dropdown title={title}>
+        <span>Content</span>
+      </Dropdown>
+    )
+  })
 
-    // expect(container.firstChild).toMatchSnapshot()
+  it('should render title', () => {
+    expect(screen.getByLabelText(/toogle dropdown/i)).toBeInTheDocument()
+  })
+  it('should handle open/close dropdown', () => {
+    const content = screen.getByText(/content/i).parentElement!
+
+    expect(content).toHaveStyle({ opacity: 0 })
+    expect(content.getAttribute('aria-hidden')).toBe('true')
+
+    userEvent.click(screen.getByLabelText(/toogle dropdown/))
+
+    expect(content).toHaveStyle({ opacity: 1 })
+    expect(content.getAttribute('aria-hidden')).toBe('false')
   })
 })
